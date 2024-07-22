64a388e162a4e5761fe4f43d9928e040d65b3365
One Code Man <pedro.lucca@live.estacio.br>
2024-07-22 11:06:33 -0300

    feature: create meeting

---
+++ app/Domain/Meeting/Actions/CreateMeetingAction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Domain\Meeting\Actions;
+
+use App\Domain\Meeting\DTOs\MeetingDTO;
+use App\Domain\Meeting\Models\Meetings;
+use App\Domain\Meeting\Models\Participants;
+use Illuminate\Support\Facades\DB;
+
+class CreateMeetingAction
+{
+    public function execute(MeetingDTO $meetingDTO)
+    {
+        return DB::transaction(function () use ($meetingDTO) {
+            $meeting = Meetings::updateOrCreate(
+                ['external_id' => $meetingDTO->external_id],
+                $meetingDTO->toArray(['accepted', 'rejected'])
+            );
+
+            $participantsList = [
+                ...$meetingDTO->accepted,
+                ...$meetingDTO->rejected
+            ];
+
+            $existentParticipants = Participants::whereIn('email', $participantsList)
+                ->where('meeting_id', $meeting->id)
+                ->pluck('email')
+                ->flip()
+                ->toArray();
+
+            $participantsToCreateData = [];
+
+            foreach ($participantsList as $participant) {
+                if (!isset($existentParticipants[$participant])) {
+                    $participantsToCreateData[] = [
+                        'email' => $participant,
+                        'meeting_id' => $meeting->id,
+                        'confirmed' => !in_array($participant, $meetingDTO->rejected),
+                    ];
+                }
+            }
+
+            if (!empty($participantsToCreateData)) {
+                Participants::insert($participantsToCreateData);
+            }
+
+            return $meeting;
+        });
+    }
+}
