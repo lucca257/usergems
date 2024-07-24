afd76b8235f5687b5c11c9ea1d310266fe8908c9
One Code Man <pedro.lucca@live.estacio.br>
2024-07-24 14:41:02 -0300

    feature: schedule command time

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 $this->load(__DIR__.'/../app/Domain/Meeting/Commands');
 
 Schedule::command('integration:meetings')->hourly();
+Schedule::command('integration:meetings-emails')->dailyAt('07:59');
